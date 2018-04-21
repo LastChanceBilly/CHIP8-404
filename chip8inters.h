6d7fa8000d42b7077842b44ca9a12d4e24701ec5
LastChanceBilly <josetecnom12@hotmail.com>
2018-04-21 19:08:11 -0400

    Delete CCHIP8.exe

---
+++ chip8inters.h
@@ -0,0 +1,29 @@
+#ifdef CHIP8INTERS_H
+#define CHIP8INTERS_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MemSize 4096
+#define StackSize 16
+#define ScreenSize 32 * 64
+
+struct chip8inters{
+	//Chip's memory
+	unsigned char *memory;
+	//Chip's Stack
+	unsigned short v[StackSize];
+	//Chip's Stack pointer
+	unsigned short SP;
+	//Chip's program counter
+	unsigned char pc;
+	//Chip's Index register
+	unsigned char I;
+	//Chip's video memory
+	unsigned char video[ScreenSize];
+	//Chip's timers
+	unsigned char delay_timer;
+	unsigned char sound_timer;
+}chip8inters;
+int dAssembler(unsigned char *buffer, int pc);
+#endif
