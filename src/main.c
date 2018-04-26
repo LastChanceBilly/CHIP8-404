845d25b3372bcd81e026664e1a9758af6b19d49a
LastChanceBilly <josetecnom12@hotmail.com>
2018-04-26 19:03:41 -0400

    Added src folder

---
+++ src/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "Chip8inters.h"
+#include "Chip8Opcodes.h"
+
+int main(int argc, char const *argv[]) {
+	Chip8 Game;
+	chip_init(&Game, "./c8games/PONG2");
+	return 0;
+}
